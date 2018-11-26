8139932ead59b23484720ad7dffb46e61bb265b8
xin.gao <xin.gao01@hand-china.com>
2018-11-26 09:31:21 +0800

    IU-2018.1.6 <Lucas@HAND-Lucas Create markdown.xml, IntelliLang.xml, project.default.xml, diff.xml, yiiguxing.translation.xml, Default.xml, applicationLibraries.xml, Tomorrow Night Eighties.icls, ignore.xml, find.xml, code.style.schemes.xml, mavenVersion.xml, javarebel.xml, Default.xml, docker.xml, debugger.renderers.xml, Tomorrow Night Bright.xml, editor.codeinsight.xml, grepConsole.xml, Tomorrow.xml, editor.xml, debugger.xml, HAND.xml, ide.general.xml, git.xml, CodeGlance.xml, Tomorrow Night Blue.xml, lombok-plugin.xml, other.xml, terminal.xml, GoogleStyle.xml, code.style.schemes, ui.lnf.xml, filetypes.xml, remote-servers.xml, plugin_ui.xml, colors.scheme.xml, databaseDrivers.xml, templates.xml, laf.xml, Class.java, jdk.table.xml, Tomorrow Night.xml, path.macros.xml, mavenRunHelper.xml, intentionSettings.xml, web-browsers.xml, proxy.settings.xml, reset.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#parse("File Header.java")
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+/**
+ * @author lucas.gao.dev@gmail.com
+ * @date ${DATE} ${TIME}
+ */
+
+public class ${NAME} {
+}
